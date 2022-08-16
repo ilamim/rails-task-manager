cdedd608ec0a0eaed76f3d78a45385b73c5226d8
Isabel Lamim <lamim@protonmail.com>
2022-08-16 14:09:56 +0200

    refacto task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    set_task
   end
 
   def new
@@ -16,21 +18,15 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def view
-  end
-
   def edit
-    set_task
   end
 
   def update
-    set_task
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    set_task
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
